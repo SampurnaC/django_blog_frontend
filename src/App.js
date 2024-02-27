baf7362724e3cd8df1372dfa52e9de29f6ac518a
sam <sampurna.chapagainnn@gmail.com>
2024-02-27 06:32:05 +0000

    make navbar and search code a global component in app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import ShowBlogs from './components/ShowBlogs';
 import AddBlog from './components/AddBlog';
@@ -9,11 +9,66 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import NavbarMenu from './components/NavbarMenu';
 import Search from './components/Search';
 
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
+
 function App() {
+
+  const url = 'http://localhost:8000/api'
+  const[data, setData] = useState([])
+  const[searchText, setsearchText] = useState("")
+
+
+  useEffect(()=>{
+    fetchData()
+  }, [searchText])
+
+  const fetchData = async()=> {
+    const endpoint = `${url}/search/?search=${searchText}`
+    try {
+      const response = await fetch(endpoint,{
+        method: 'GET'
+      })
+      const data = await response.json()
+      setData(data)
+    }
+    catch(e){
+      console.log(e)
+    }
+  }
+
+
   return (
     <div className="App">
       <Router>
-        {/* <NavbarMenu /> */}
+      <NavbarMenu searchText={searchText} setsearchText={setsearchText}/>
+      {searchText ? 
+        <Container>
+          <h2>Searched Blogs</h2>
+          <Row>
+            {
+              data.map((blog,index)=>(
+                <Card style={{ width: '18rem' }} className=" ms-2 my-2">
+                  <Col>
+                    <Card.Img variant="top" src={`http://localhost:8000${blog.image}/`} />
+                    <Card.Body>
+                      <Card.Title>{blog.name}</Card.Title>
+                      <Card.Text>{blog.description}</Card.Text>
+                    </Card.Body>
+                    <Link className='btn btn-dark' to={`/${blog.id}/`}>Show Blog</Link>
+                  </Col>
+                </Card>
+              ))
+              
+            }
+          </Row>
+        </Container> : "" }
+        
         <Routes>
           <Route exact path="/" element={<ShowBlogs />} />
           <Route exact path="/addBlog" element={<AddBlog />} />
