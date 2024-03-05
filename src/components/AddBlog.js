d1e632d5ae7c59977fef5e43aaa695f81b864257
sam <sampurna.chapagainnn@gmail.com>
2024-03-05 13:52:07 +0000

    fetch categories from api and add dropdown to list categories on add form

--- src/components/AddBlog.js
+++ src/components/AddBlog.js
@@ -1,38 +1,55 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const AddBlog = () => {
 
     const [image, setImage] = useState([])
     const [name, setName] = useState("")
     const [description, setDescription] = useState("")
-
+    const [categories, setCategories] = useState([])
+    const [category, setCategory] = useState("")
     const navigate = useNavigate();
 
     const addNewBlog = async() => {
-        let formField = new FormData()
+      let formField = new FormData()
 
-        formField.append('name', name)
-        formField.append('description', description)
+      formField.append('name', name)
+      formField.append('description', description)
+      formField.append('category', category)
 
-        if(image !== null) {
-            formField.append('image', image)
-        }
 
-        await axios({
-            method: 'post',
-            url: 'http://localhost:8000/api/blog-create/',
-            data: formField
-        }).then((response) => {
-            console.log(response.data)
-            navigate('/')
-        })
+      if(image !== null) {
+          formField.append('image', image)
+      }
+
+      await axios({
+          method: 'post',
+          url: 'http://localhost:8000/api/blog-create/',
+          data: formField
+      }).then((response) => {
+        console.log(response)
+          navigate('/')
+      })
+
+
     }
 
+    useEffect(()=>{
+      const getCategories = async()=>{
+        const res = await fetch('http://localhost:8000/api/category-list/')
+        const data=await res.json()
+        setCategories(data)
 
+      }
+      getCategories()
+
+    },[])
+    
     return (
       <div className="container">
+        
         <div className="w-75 mx-auto shadow p-5 mx-auto my-5">
             <h2 className="text-center mb-4">Add A Blog</h2>
             
@@ -64,10 +81,28 @@ const AddBlog = () => {
               required
               />
             </div>
+           
+            
+            <label>Choose a car:</label>
+              <select value={category} onChange={(e)=> setCategory(e.target.value)}>
+              {categories.map((cat,index)=>(
+                <option value={cat.id}>{cat.name}</option>
+                ))
+                }
+              </select>
+            <p>You selected: {category}</p>
+            {/* {
+              category.map((cat,index)=>(
+                <h1>{cat.name}</h1>
+              ))
+            } */}
+            
             <br />
             <button className="btn btn-primary btn-block" onClick={addNewBlog}>Add Blog</button>
         
         </div>
+
+        
       </div>
   );
 }
